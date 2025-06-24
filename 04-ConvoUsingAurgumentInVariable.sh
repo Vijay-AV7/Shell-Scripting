14393e29f914365e8d0bcf02b52aa0e2d8639715
Vijay <gkpk222@gmail.com>
2025-06-24 12:59:45 +0530

    Variable Convo with Aruguments

--- 04-ConvoUsingAurgumentInVariable.sh
+++ 04-ConvoUsingAurgumentInVariable.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: Hello $PERSON2!"
+echo "${PERSON1}:: Hello ${PERSON2}"
 echo "$PERSON2:: Helllo $PERSON1! how are you?"
 echo "$PERSON1:: I am good, how are you $PERSON2?"
 echo "$PERSON2:: I am doing good"
