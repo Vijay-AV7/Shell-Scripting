15d476a3b496fb8c2e8dfdce2d393e1e0df69e78
Vijay <gkpk222@gmail.com>
2025-06-26 08:39:37 +0530

    User credentials without hiding

---
+++ 05-UserCredentials.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter your name -"
+read USERNAME # here USERNAME name is a variable name
+
+echo "Please enter your password"
+read PASSWORD # here PASSWORD name is a variable name
