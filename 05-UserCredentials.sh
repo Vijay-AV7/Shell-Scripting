b3f76e473ff2e3947cb5ec3b82872ec112aa1270
Vijay <gkpk222@gmail.com>
2025-06-26 09:07:00 +0530

    -m to -s

--- 05-UserCredentials.sh
+++ 05-UserCredentials.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your name -"
-read -m USERNAME # here USERNAME name is a variable name
+read -s USERNAME # here USERNAME name is a variable name
 echo "$USERNAME"
 
 echo "Please enter your password"
-read -m PASSWORD # here PASSWORD name is a variable name
+read -s PASSWORD # here PASSWORD name is a variable name
 echo "$PASSWORD"
