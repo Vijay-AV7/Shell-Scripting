4ea05337d9be4e9a9598f764121651d0e801406f
Vijay <gkpk222@gmail.com>
2025-06-28 10:51:25 +0530

    First commit

---
+++ 09-SpecialVariables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LYTCYAN=\e[96m
+WHITE=\e[0m
+
+echo -e "Variable passed - $LYTCYAN $@ $WHITE"
+echo -e "Number Of Variables Passed - $LYTCYAN $# $WHITE"
+echo -e "File name - $LYTCYAN $0 $WHITE"
+echo -e "Presnet Working Directtory - $LYTCYAN $PWD $WHITE"
+echo -e "User Home Directory - $LYTCYAN $HOME $WHITE"
+echo -e "Present Command Process Id  - $LYTCYAN $$ $WHITE"
+echo -e "Previous Command Process Id - $LYTCYAN $! $WHITE"
