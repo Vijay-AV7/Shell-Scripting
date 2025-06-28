892236179b29bd35f2f00d41864f28c4aadadc8e
Vijay <gkpk222@gmail.com>
2025-06-28 10:30:00 +0530

    syntax corrected

--- 07-Arrays.sh
+++ 07-Arrays.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Comments - shebang is an intrepreter to run and execute the shell scripting commands
 
-MOVIES=("OG" "WAR2" "F1" "COOLIE")
+MOVIES=("OG" "WAR2" "F1" "COOLIE" "$1")
 
 echo "Pawan Kalyan movie - ${MOVIES[0]}"
 echo "Rajini movie - ${MOVIES[3]}"
 echo "Bradd Pitt movie - ${MOVIES[2]}"
 echo "Hritik Roshan movie - ${MOVIES[1]}"
 
-echo -e "This month movie releases - \e[33m ${MOVIES[@]}"
+echo -e "This month movie releases - \e[33m ${MOVIES[@]} \e[77m $@"
