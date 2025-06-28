3219a34ca6f2023f0c0b09877981f178d0db0af9
Vijay <gkpk222@gmail.com>
2025-06-28 10:24:02 +0530

    syntax corrected

--- 07-Arrays.sh
+++ 07-Arrays.sh
@@ -3,9 +3,9 @@
 
 MOVIES=("OG" "WAR2" "F1" "COOLIE")
 
-echo "Pawan Kalyan movie - $MOVIES[0]"
-echo "Rajini movie - $MOVIES[3]"
-echo "Bradd Pitt movie - $MOVIES[2]"
-echo "Hritik Roshan movie - $MOVIES[1]"
+echo "Pawan Kalyan movie - ${MOVIES[0]}"
+echo "Rajini movie - ${MOVIES[3]}"
+echo "Bradd Pitt movie - ${MOVIES[2]}"
+echo "Hritik Roshan movie - ${MOVIES[1]}"
 
 echo -e "This month movie releases - \e[30m $@"
