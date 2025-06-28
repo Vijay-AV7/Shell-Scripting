b7b32b3323ea8469316dfe605b24c720d7590a17
Vijay <gkpk222@gmail.com>
2025-06-28 10:08:54 +0530

    06-AddingTwoNumbers & 07-Arrays

---
+++ 07-Arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Comments - shebang is an intrepreter to run and execute the shell scripting commands
+
+MOVIES=("OG" "WAR2" "F1" "COOLIE")
+
+echo "Pawan Kalyan movie - $MOVIES[0]"
+echo "Rajini movie - $MOVIES[3]"
+echo "Bradd Pitt movie - $MOVIES[2]"
+echo "Hritik Roshan movie - $MOVIES[1]"
+
+echo -e "This month movie releases - \e[30m $@"
